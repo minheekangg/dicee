5727cfa7bcdf035750745b4406f8157163e9fb29
minheekangg <minheekangg@gmail.com>
2019-01-31 18:46:12 -0500

    generate random number upon button click

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2  : Int = 0
 
+    @IBOutlet weak var DiceImageView1: UIImageView!
+    @IBOutlet weak var DiceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func RollButtonPressed(_ sender: UIButton) {
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        print(randomDiceIndex1)
+    }
+    
 }
 
