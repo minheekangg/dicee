704496ff5d016bb48e3885c36aca0790084ef55a
minheekangg <minheekangg@gmail.com>
2019-01-31 19:04:26 -0500

    done

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -12,9 +12,11 @@ class ViewController: UIViewController {
     
     var randomDiceIndex1 : Int = 0
     var randomDiceIndex2  : Int = 0
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
 
-    @IBOutlet weak var DiceImageView1: UIImageView!
-    @IBOutlet weak var DiceImageView2: UIImageView!
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +26,8 @@ class ViewController: UIViewController {
     @IBAction func RollButtonPressed(_ sender: UIButton) {
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
-        print(randomDiceIndex1)
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
     
 }
